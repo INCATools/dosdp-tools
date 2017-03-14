d152185f73acd1cd34af0b0b97d7c42f0596b4aa
Jim Balhoff <jim@balhoff.org>
2017-03-14 11:56:41 -0400

    Cleanup.

--- src/test/scala/org/monarchinitiative/dosdp/QueryGeneratorTest.scala
+++ src/test/scala/org/monarchinitiative/dosdp/QueryGeneratorTest.scala
@@ -12,21 +12,17 @@ import scala.collection.JavaConverters._
 class QueryGeneratorTest extends UnitSpec {
 
   "Defined class" should "be first column" in {
-    var processed = false
-    for {
+    val dosdp = (for {
       json <- Parser.parse(new InputStreamReader(getClass.getResourceAsStream("QueryGeneratorTest.yaml")))
-      dosdp <- json.as[DOSDP]
-    } {
-      val prefixes = (for {
-        prefixesJson <- Parser.parse(new InputStreamReader(getClass.getResourceAsStream("StandardPrefixes.yaml"))).toOption
-        prefixMap <- prefixesJson.as[Map[String, String]].toOption
-      } yield prefixMap).getOrElse(Map.empty)
-      val variables = QueryFactory.create(SPARQL.queryFor(ExpandedDOSDP(dosdp, prefixes))).getProjectVars.asScala
-      variables(0).getVarName shouldEqual "defined_class"
-      variables(1).getVarName shouldEqual "defined_class_label"
-      processed = true
-    }
-    processed shouldEqual true
+      pattern <- json.as[DOSDP]
+    } yield pattern).getOrElse(???)
+    val prefixes = (for {
+      prefixesJson <- Parser.parse(new InputStreamReader(getClass.getResourceAsStream("StandardPrefixes.yaml"))).toOption
+      prefixMap <- prefixesJson.as[Map[String, String]].toOption
+    } yield prefixMap).getOrElse(Map.empty)
+    val variables = QueryFactory.create(SPARQL.queryFor(ExpandedDOSDP(dosdp, prefixes))).getProjectVars.asScala
+    variables(0).getVarName shouldEqual "defined_class"
+    variables(1).getVarName shouldEqual "defined_class_label"
   }
 
 }
