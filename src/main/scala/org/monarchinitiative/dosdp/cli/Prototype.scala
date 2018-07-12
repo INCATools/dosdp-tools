3566dcc8a9a53abac536268adaadb1ec4d291a8f
Jim Balhoff <jim@balhoff.org>
2018-07-12 14:29:31 -0400

    Generate prototype ontology for folder of DOSDPs.

--- src/main/scala/org/monarchinitiative/dosdp/cli/Prototype.scala
+++ src/main/scala/org/monarchinitiative/dosdp/cli/Prototype.scala
@@ -1,7 +1,7 @@
 package org.monarchinitiative.dosdp.cli
 
-import java.io.File
-
+import better.files._
+import File._
 import scala.collection.JavaConverters._
 
 import org.backuity.clist._
@@ -11,19 +11,37 @@ import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat
 import org.semanticweb.owlapi.model.IRI
 import org.semanticweb.owlapi.model.OWLAxiom
 
-object Prototype extends Command(description = "output \"prototype\" axioms using default fillers for a pattern") with Common {
+object Prototype extends Command(description = "output \"prototype\" axioms using default fillers for a pattern or folder of patterns") with Common {
 
   def run: Unit = {
-    val dosdp = inputDOSDP
-    val fillers = dosdp.vars.getOrElse(Map.empty) ++
-      dosdp.list_vars.getOrElse(Map.empty) ++
-      dosdp.data_vars.getOrElse(Map.empty) ++
-      dosdp.data_list_vars.getOrElse(Map.empty) +
-      (DOSDP.DefinedClassVariable -> dosdp.pattern_iri.getOrElse(IRI.create(new File(templateFile)).toString)) //FIXME this is temporary until pattern_iri is standard; won't work with pattern specified by URI
-    val axioms: Set[OWLAxiom] = Generate.renderPattern(dosdp, prefixes, fillers, ontologyOpt, true, true)
+    val possibleFile = File(templateFile)
+    val filenames = if (possibleFile.isDirectory) {
+      possibleFile.list.filter { f =>
+        println(f)
+        f.extension(false, false, true).map { ext =>
+          (ext == "yaml") || (ext == "yml")
+        }.getOrElse(false)
+      }.map(_.toString).toSet
+    } else Set(templateFile)
+    println(filenames)
+    val dosdps = filenames.map(inputDOSDPFrom)
+    val axioms = for {
+      dosdp <- dosdps
+      axiom <- axiomsFor(dosdp)
+    } yield axiom
     val manager = OWLManager.createOWLOntologyManager()
     val ont = manager.createOntology(axioms.asJava)
     manager.saveOntology(ont, new FunctionalSyntaxDocumentFormat(), IRI.create(outfile))
   }
+
+  private def axiomsFor(dosdp: DOSDP): Set[OWLAxiom] =
+    dosdp.pattern_iri.map { iri =>
+      val fillers = dosdp.vars.getOrElse(Map.empty) ++
+        dosdp.list_vars.getOrElse(Map.empty) ++
+        dosdp.data_vars.getOrElse(Map.empty) ++
+        dosdp.data_list_vars.getOrElse(Map.empty) +
+        (DOSDP.DefinedClassVariable -> iri)
+      Generate.renderPattern(dosdp, prefixes, fillers, ontologyOpt, true, true)
+    }.toSet.flatten
 
 }
