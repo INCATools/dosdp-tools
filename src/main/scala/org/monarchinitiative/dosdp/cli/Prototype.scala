337ef15eebf4a83e9eab1c7a7fe1cf21ccfd401f
Jim Balhoff <jim@balhoff.org>
2018-07-31 11:43:42 -0400

    Add annotation for pattern name to prototype classes. Fixes #53.

--- src/main/scala/org/monarchinitiative/dosdp/cli/Prototype.scala
+++ src/main/scala/org/monarchinitiative/dosdp/cli/Prototype.scala
@@ -1,17 +1,22 @@
 package org.monarchinitiative.dosdp.cli
 
-import better.files._
-import File._
 import scala.collection.JavaConverters._
 
 import org.backuity.clist._
 import org.monarchinitiative.dosdp._
+import org.openrdf.model.vocabulary.DCTERMS
+import org.phenoscape.scowl._
 import org.semanticweb.owlapi.apibinding.OWLManager
 import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat
 import org.semanticweb.owlapi.model.IRI
 import org.semanticweb.owlapi.model.OWLAxiom
 
+import better.files._
+import better.files.File._
+
 object Prototype extends Command(description = "output \"prototype\" axioms using default fillers for a pattern or folder of patterns") with Common {
+
+  val DCTTitle = AnnotationProperty(DCTERMS.TITLE.stringValue)
 
   def run: Unit = {
     val possibleFile = File(templateFile)
@@ -41,7 +46,7 @@ object Prototype extends Command(description = "output \"prototype\" axioms usin
         dosdp.data_vars.getOrElse(Map.empty) ++
         dosdp.data_list_vars.getOrElse(Map.empty) +
         (DOSDP.DefinedClassVariable -> iri)
-      Generate.renderPattern(dosdp, prefixes, fillers, ontologyOpt, true, true)
+      Generate.renderPattern(dosdp, prefixes, fillers, ontologyOpt, true, true) ++ dosdp.pattern_name.map(name => Class(iri) Annotation (DCTTitle, name))
     }.toSet.flatten
 
 }
