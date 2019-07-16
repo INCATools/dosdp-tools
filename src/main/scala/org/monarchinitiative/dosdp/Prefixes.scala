6743747b5db18eef6168d7480556e2e1fccdda8e
Jim Balhoff <jim@balhoff.org>
2019-07-16 12:58:10 -0400

    Allow parsing of datatype restrictions. Fixes #142.

--- src/main/scala/org/monarchinitiative/dosdp/Prefixes.scala
+++ src/main/scala/org/monarchinitiative/dosdp/Prefixes.scala
@@ -8,6 +8,7 @@ object OBOPrefixes extends PartialFunction[String, String] {
     "rdf" -> "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "rdfs" -> "http://www.w3.org/2000/01/rdf-schema#",
     "owl" -> "http://www.w3.org/2002/07/owl#",
+    "xsd" -> "http://www.w3.org/2001/XMLSchema#",
     "dc" -> "http://purl.org/dc/elements/1.1/",
     "dct" -> "http://purl.org/dc/terms/",
     "skos" -> "http://www.w3.org/2004/02/skos/core#",
