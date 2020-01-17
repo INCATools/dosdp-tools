940221282578cafa483dd18ddb68148259e8e5b1
Jim Balhoff <jim@balhoff.org>
2020-01-17 11:49:14 -0500

    Update test for new return format.

--- src/test/scala/org/monarchinitiative/dosdp/MissingValuesTest.scala
+++ src/test/scala/org/monarchinitiative/dosdp/MissingValuesTest.scala
@@ -9,7 +9,7 @@ class MissingValuesTest extends UnitSpec {
 
   "Missing columns and cell values" should "be handled by dropping outputs" in {
     val dosdp = Generate.inputDOSDPFrom("src/test/resources/org/monarchinitiative/dosdp/missing_values_test.yaml")
-    val fillers = Generate.readFillers(new File("src/test/resources/org/monarchinitiative/dosdp/missing_values_test.tsv"), new TSVFormat {})
+    val (_, fillers) = Generate.readFillers(new File("src/test/resources/org/monarchinitiative/dosdp/missing_values_test.tsv"), new TSVFormat {})
     val axioms = Generate.renderPattern(dosdp: DOSDP, OBOPrefixes, fillers, None, true, true, None, false)
     // No exceptions should be thrown
   }
