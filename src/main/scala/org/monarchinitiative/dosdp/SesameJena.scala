b7533fd694d7bf2112a28af69bf8eaaf0965fac2
Jim Balhoff <jim@balhoff.org>
2020-08-20 14:25:39 -0400

    Update to OWL API 4.5.17, which requires changing from OpenRDF to rdf4j.

--- src/main/scala/org/monarchinitiative/dosdp/SesameJena.scala
+++ src/main/scala/org/monarchinitiative/dosdp/SesameJena.scala
@@ -3,8 +3,8 @@ package org.monarchinitiative.dosdp
 import org.apache.jena.datatypes.TypeMapper
 import org.apache.jena.rdf.model.impl.ResourceImpl
 import org.apache.jena.rdf.model.{AnonId, ResourceFactory, Statement}
-import org.openrdf.model.{BNode, Literal, URI, Statement => SesameStatement}
-import org.openrdf.rio.helpers.StatementCollector
+import org.eclipse.rdf4j.model.{BNode, IRI, Literal, URI, Statement => SesameStatement}
+import org.eclipse.rdf4j.rio.helpers.StatementCollector
 import org.semanticweb.owlapi.model.OWLOntology
 import org.semanticweb.owlapi.rio.RioRenderer
 
@@ -21,13 +21,15 @@ object SesameJena {
   def sesameTripleToJena(triple: SesameStatement): Statement = {
     val subject = triple.getSubject match {
       case bnode: BNode => new ResourceImpl(new AnonId(bnode.getID))
+      case uri: IRI     => ResourceFactory.createResource(uri.stringValue)
       case uri: URI     => ResourceFactory.createResource(uri.stringValue)
     }
     val predicate = ResourceFactory.createProperty(triple.getPredicate.stringValue)
     val obj = triple.getObject match {
       case bnode: BNode                                    => new ResourceImpl(new AnonId(bnode.getID))
+      case uri: IRI                                        => ResourceFactory.createResource(uri.stringValue)
       case uri: URI                                        => ResourceFactory.createResource(uri.stringValue)
-      case literal: Literal if literal.getLanguage != null => ResourceFactory.createLangLiteral(literal.getLabel, literal.getLanguage)
+      case literal: Literal if literal.getLanguage.isPresent => ResourceFactory.createLangLiteral(literal.getLabel, literal.getLanguage.get)
       case literal: Literal if literal.getDatatype != null => ResourceFactory.createTypedLiteral(literal.getLabel, TypeMapper.getInstance.getSafeTypeByName(literal.getDatatype.stringValue))
       case literal: Literal                                => ResourceFactory.createStringLiteral(literal.getLabel)
     }
