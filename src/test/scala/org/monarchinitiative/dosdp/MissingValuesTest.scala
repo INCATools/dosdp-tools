f2eb5b55f63fc1bf305f63cacd994d9dc1daaea2
Jason Reilly <jdr0887@gmail.com>
2020-12-02 11:29:47 -0500

    adding testMissingColumnsAndCellValuesFromCSV

--- src/test/scala/org/monarchinitiative/dosdp/MissingValuesTest.scala
+++ src/test/scala/org/monarchinitiative/dosdp/MissingValuesTest.scala
@@ -2,14 +2,19 @@ package org.monarchinitiative.dosdp
 
 import java.io.File
 
-import com.github.tototoshi.csv.TSVFormat
+import com.github.tototoshi.csv.{DefaultCSVFormat, TSVFormat}
 import org.monarchinitiative.dosdp.cli.{Config, Generate}
+import org.semanticweb.owlapi.model.{OWLAnnotationAssertionAxiom, OWLClass}
 import zio.test.Assertion._
 import zio.test._
+import org.phenoscape.scowl._
 
 object MissingValuesTest extends DefaultRunnableSpec {
 
-  def spec = suite("Missing columns and cell values") {
+  val term: OWLClass = Class("http://purl.obolibrary.org/obo/EX_0001")
+  val term0001: OWLAnnotationAssertionAxiom = term Annotation(RDFSLabel, "Term 0001")
+
+  val testMissingColumnsAndCellValuesFromTSV = suite("Missing columns and cell values from TSV") {
     testM("Missing columns and cell values should be handled by dropping outputs") {
       for {
         dosdp <- Config.inputDOSDPFrom("src/test/resources/org/monarchinitiative/dosdp/missing_values_test.yaml")
@@ -17,8 +22,24 @@ object MissingValuesTest extends DefaultRunnableSpec {
         (_, fillers) = columnsAndFillers
         // should not fail from missing values
         axioms <- Generate.renderPattern(dosdp: DOSDP, OBOPrefixes, fillers, None, true, true, None, false, AxiomRestrictionsTest.OboInOwlSource, false)
-      } yield assert(axioms)(isNonEmpty)
+        //_ = axioms.foreach(a => println(a))
+      } yield assert(axioms)(isNonEmpty) && assert(axioms)(contains(term0001))
     }
   }
+
+  val testMissingColumnsAndCellValuesFromCSV = suite("Missing columns and cell values from CSV") {
+    testM("Missing columns and cell values should be handled by dropping outputs") {
+      for {
+        dosdp <- Config.inputDOSDPFrom("src/test/resources/org/monarchinitiative/dosdp/missing_values_test.yaml")
+        columnsAndFillers <- Generate.readFillers(new File("src/test/resources/org/monarchinitiative/dosdp/missing_values_test.csv"), new DefaultCSVFormat {})
+        (_, fillers) = columnsAndFillers
+        // should not fail from missing values
+        axioms <- Generate.renderPattern(dosdp: DOSDP, OBOPrefixes, fillers, None, true, true, None, false, AxiomRestrictionsTest.OboInOwlSource, false)
+        //_ = axioms.foreach(a => println(a))
+      } yield assert(axioms)(isNonEmpty) && assert(axioms)(contains(term0001))
+    }
+  }
+
+  def spec = suite("All tests")(testMissingColumnsAndCellValuesFromTSV, testMissingColumnsAndCellValuesFromCSV)
 
 }
