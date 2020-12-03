dfcd7722cfdbc4ed08453d2532de47850f4651f4
Jason Reilly <jdr0887@gmail.com>
2020-12-03 08:38:39 -0500

    adding assertion that text is trimmed despite trailing space in template

--- src/test/scala/org/monarchinitiative/dosdp/MissingValuesTest.scala
+++ src/test/scala/org/monarchinitiative/dosdp/MissingValuesTest.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import com.github.tototoshi.csv.{DefaultCSVFormat, TSVFormat}
 import org.monarchinitiative.dosdp.cli.{Config, Generate}
-import org.semanticweb.owlapi.model.{OWLAnnotationAssertionAxiom, OWLClass}
+import org.semanticweb.owlapi.model.{OWLAnnotationAssertionAxiom, OWLAnnotationProperty, OWLClass}
 import zio.test.Assertion._
 import zio.test._
 import org.phenoscape.scowl._
@@ -13,6 +13,9 @@ object MissingValuesTest extends DefaultRunnableSpec {
 
   val term: OWLClass = Class("http://purl.obolibrary.org/obo/EX_0001")
   val term0001: OWLAnnotationAssertionAxiom = term Annotation(RDFSLabel, "Term 0001")
+
+  val oboIAO115: OWLAnnotationProperty = AnnotationProperty("http://purl.obolibrary.org/obo/IAO_0000115")
+  val termSubstituteMunged: OWLAnnotationAssertionAxiom = term Annotation(oboIAO115, "Term 0001 Arm two and then one")
 
   val testMissingColumnsAndCellValuesFromTSV = suite("Missing columns and cell values from TSV") {
     testM("Missing columns and cell values should be handled by dropping outputs") {
@@ -23,7 +26,7 @@ object MissingValuesTest extends DefaultRunnableSpec {
         // should not fail from missing values
         axioms <- Generate.renderPattern(dosdp: DOSDP, OBOPrefixes, fillers, None, true, true, None, false, AxiomRestrictionsTest.OboInOwlSource, false)
         //_ = axioms.foreach(a => println(a))
-      } yield assert(axioms)(isNonEmpty) && assert(axioms)(contains(term0001))
+      } yield assert(axioms)(isNonEmpty) && assert(axioms)(contains(term0001)) && assert(axioms)(contains(termSubstituteMunged))
     }
   }
 
@@ -36,7 +39,7 @@ object MissingValuesTest extends DefaultRunnableSpec {
         // should not fail from missing values
         axioms <- Generate.renderPattern(dosdp: DOSDP, OBOPrefixes, fillers, None, true, true, None, false, AxiomRestrictionsTest.OboInOwlSource, false)
         //_ = axioms.foreach(a => println(a))
-      } yield assert(axioms)(isNonEmpty) && assert(axioms)(contains(term0001))
+      } yield assert(axioms)(isNonEmpty) && assert(axioms)(contains(term0001)) && assert(axioms)(contains(termSubstituteMunged))
     }
   }
 
