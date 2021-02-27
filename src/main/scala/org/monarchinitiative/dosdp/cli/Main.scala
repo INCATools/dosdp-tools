47775b337f02f1211a00422ad9e41fe262d0e6ae
Jason Reilly <jdr0887@gmail.com>
2021-02-27 14:46:31 -0500

    adding duration, catching DOSDPErrors

--- src/main/scala/org/monarchinitiative/dosdp/cli/Main.scala
+++ src/main/scala/org/monarchinitiative/dosdp/cli/Main.scala
@@ -4,6 +4,10 @@ import caseapp._
 import org.apache.jena.sys.JenaSystem
 import scribe.Level
 import zio._
+import zio.console.putStrLn
+
+import java.time.{Duration, Instant}
+import java.util.concurrent.TimeUnit
 
 object Main extends ZCommandApp[Config] {
 
@@ -11,9 +15,26 @@ object Main extends ZCommandApp[Config] {
 
   override def progName: String = "dosdp-tools"
 
-  override def run(config: Config, args: RemainingArgs): ZIO[ZEnv, Nothing, ExitCode] =
-    ZIO.effectTotal(JenaSystem.init()) *>
-      ZIO.effectTotal(scribe.Logger.root.clearHandlers().clearModifiers().withHandler(minimumLevel = Some(Level.Info)).replace()) *>
-      config.run.exitCode
+  def toHumanReadableDateDiff(start: Long, end: Long): String = {
+    val seconds = Duration.between(Instant.ofEpochMilli(start), Instant.ofEpochMilli(end)).getSeconds
+    String.format("%d:%02d:%02d", seconds / 3600, (seconds % 3600) / 60, seconds % 60);
+  }
+
+  override def run(config: Config, args: RemainingArgs): ZIO[ZEnv, Nothing, ExitCode] = {
+    val program = for {
+      start <- clock.currentTime(TimeUnit.MILLISECONDS)
+      _ <- ZIO.effectTotal(JenaSystem.init()) *> ZIO.effectTotal(
+        scribe.Logger.root.clearHandlers().clearModifiers().withHandler(minimumLevel = Some(Level.Info)).replace())
+      exitCode <- config.run.exitCode
+      end <- clock.currentTime(TimeUnit.MILLISECONDS)
+      duration = toHumanReadableDateDiff(start, end)
+      _ <- ZIO.effect(scribe.info(s"Duration: $duration"))
+    } yield exitCode
+    program
+      .catchSome { case DOSDPError(msg, e) =>
+        ZIO.effectTotal(scribe.error(msg)).as(ExitCode.failure)
+      }
+      .catchAllCause(cause => putStrLn(cause.untraced.prettyPrint).as(ExitCode.failure))
+  }
 
 }
