8e334d0860c843ac86185dc60670e6c556d31396
Jim Balhoff <jim@balhoff.org>
2021-12-02 10:28:37 -0500

    Alphabetize docs index.

--- src/main/scala/org/monarchinitiative/dosdp/DocsMarkdown.scala
+++ src/main/scala/org/monarchinitiative/dosdp/DocsMarkdown.scala
@@ -11,12 +11,13 @@ object DocsMarkdown {
 
   def markdown(edosdp: ExpandedDOSDP, docData: DocData, renderer: OWLObjectRenderer, data: List[List[String]]): String = {
     def r(obj: OWLObject): String = renderer.render(obj).replace("\n", " ")
+
     val dosdp = edosdp.dosdp
     val PatternIRI = dosdp.pattern_iri.map(IRI.create).getOrElse(IRI.create("http://example.org/"))
     val PatternCls = Class(PatternIRI)
     val equivPrefix = if (docData.equivalentTo.flatMap(_.getClassExpressionsMinus(PatternCls).asScala).size > 1) "- " else ""
     val subClassOfPrefix = if (docData.subClassOf.size > 1) "- " else ""
-    val variables = ((edosdp.varExpressions.to(List) ::: edosdp.listVarExpressions.to(List)).map { case (k,v) => k -> r(v) }) :::
+    val variables = ((edosdp.varExpressions.to(List) ::: edosdp.listVarExpressions.to(List)).map { case (k, v) => k -> r(v) }) :::
       dosdp.data_vars.getOrElse(Map.empty).to(List) ::: dosdp.data_list_vars.getOrElse(Map.empty).to(List)
 
     s"""# ${dosdp.pattern_name.getOrElse("")}
@@ -34,28 +35,31 @@ ${dosdp.contributors.getOrElse(Nil).map(c => s"- $c").mkString("\n")}
 
 | Variable name | Allowed type |
 |:--------------|:-------------|
-${variables.map {case (name, range) => s"| `{$name}` | $range |" }.mkString("\n")}
+${variables.map { case (name, range) => s"| `{$name}` | $range |" }.mkString("\n")}
 
 ## Name
 
-${docData.name.map { ax => r(ax.getValue)}.mkString("\n\n")}
+${docData.name.map { ax => r(ax.getValue) }.mkString("\n\n")}
 
 ## Annotations
 
-${docData.annotations.map { ax =>
+${
+      docData.annotations.map { ax =>
         s"- ${r(ax.getProperty)}: ${r(ax.getValue)}"
-    }.mkString("\n")
-}
+      }.mkString("\n")
+    }
 
 ## Definition
 
-${docData.definition.map { ax => r(ax.getValue)}.mkString("\n\n")}
+${docData.definition.map { ax => r(ax.getValue) }.mkString("\n\n")}
 
 ## Equivalent to
 
-${docData.equivalentTo.flatMap { ax =>
-      ax.getClassExpressionsMinus(PatternCls).asScala.map(cls => r(cls)).map(text => s"$equivPrefix$text")
-    }.mkString("\n")}
+${
+      docData.equivalentTo.flatMap { ax =>
+        ax.getClassExpressionsMinus(PatternCls).asScala.map(cls => r(cls)).map(text => s"$equivPrefix$text")
+      }.mkString("\n")
+    }
 
 ${if (docData.subClassOf.nonEmpty) "## Subclass of\n" else ""}
 ${docData.subClassOf.map(ax => r(ax.getSuperClass)).map(text => s"$subClassOfPrefix$text").mkString("\n")}
@@ -75,11 +79,12 @@ ${data.drop(1).map(_.mkString("| ", " | ", " |")).mkString("\n")}
   }
 
   def indexMarkdown(patterns: List[(DOSDP, String)]): String = {
-s"""# Design Patterns
+    val sorted = patterns.sortBy(_._1.pattern_name)
+    s"""# Design Patterns
 
 | Pattern | Description |
 |:--------|:------------|
-${patterns.map { case (p, filename) => s"| [${p.pattern_name.getOrElse("*unnamed*")}]($filename) | ${p.description.getOrElse("*no description*")} |" }.mkString("\n")}
+${sorted.map { case (p, filename) => s"| [${p.pattern_name.getOrElse("*unnamed*")}]($filename) | ${p.description.getOrElse("*no description*")} |" }.mkString("\n")}
 """
   }
 
